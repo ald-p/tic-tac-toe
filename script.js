97782e5a882bb6a15a5bf4904ed0d824a4f65e6e
Aldwin <aldwin.pag@gmail.com>
2022-10-25 00:03:27 -0400

    Allow user to click on button and display player sign in correct location

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
+const Player = (sign, locationPlayed) => {
+  this.sign = sign;
+  this.locationPlayed = locationPlayed
+
+  return {sign, locationPlayed};
+}
+
 const gameBoard = (() => {
   const board = ['','','','','','','','',''];
 
-  return { board };
+  const updateGameBoard = (player) => {
+    board.splice(player.locationPlayed, 0, player.sign);
+    return board;
+  }
+
+  return { updateGameBoard };
 })();
 
-const Player = (sign) => {
-  this.sign = sign;
+const gameController = (() => {
+  const playerX = Player('X', '');
+  const playerO = Player('O', '');
 
-  return {sign};
-}
+  let currentPlayer = playerX; // initialize current player
+
+  const playRound = (e) => {
+    currentPlayer.locationPlayed = Number(e.target.dataset.val);
+    console.log(currentPlayer, playerX);
+    const updatedBoard = gameBoard.updateGameBoard(currentPlayer);
+    displayController.displayBoardContent(updatedBoard);
+  }
+
+  return { playRound };
+})();
 
 const displayController = (() => {
   const currentPlayerEl = document.querySelector('.current-player');
   const gameBoardEl = document.querySelectorAll('.game-btn');
 
+  gameBoardEl.forEach( btn => btn.addEventListener('click', gameController.playRound, false));
+
   const displayBoardContent = (boardArr) => {
     gameBoardEl.forEach( (btn, idx) => btn.textContent = boardArr[idx]);
   }
+  
+  return { displayBoardContent };
+
 })();
 
-const gameController = (() => {
-  const playerX = Player('X');
-  const playerO = Player('O');
-  
-})();
